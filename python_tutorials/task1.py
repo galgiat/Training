9fc727fe3de16e0a5d9b5f59dea400f3996efcc2
Gal Giat <galgiat122@gmail.com>
2026-09-02 18:53:51 +0300

    update the first task

--- python_tutorials/task1.py
+++ python_tutorials/task1.py
@@ -1,1 +1,12 @@
-print("hello world")
+
+
+def print_count(count: int) -> None:
+
+    """
+    """
+    print (f"{count=}")
+
+
+if __name__ == "__main__":
+    print("hello world")
+    print_count(10)
